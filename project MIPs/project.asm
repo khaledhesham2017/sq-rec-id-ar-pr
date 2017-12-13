6154e01cd7132754bafa26cdcc8f4174370f993f
Belal <billy.fcih@yahoo.com>
2017-12-13 20:43:49 +0200

    Change the datatypes to double also adjust the comments

--- project MIPs/project.asm
+++ project MIPs/project.asm
@@ -4,97 +4,98 @@
 main:
 	# Get inputs from user 
     # First input
-    # Print messege  'Enter the shape width: ''
+    # Prints 'Enter the shape width: '
 	li $v0, 4		# print_string syscall code = 4
 	la $a0, msg1	# load the address of msg
 	syscall
     # Get width from user and save
-	li $v0, 5		# read_int syscall code = 5
-	syscall	
-	move $a1, $v0	# syscall results returned in $v0
-    # Second input
+	li $v0, 7		# read_double  syscall code = 7
+	syscall	        # system_call
+	mov.d $f14, $f0	# syscall results returned in $f14
+	# Second input
     # Prints 'Enter the shape length: '
 	li $v0, 4		# print_string syscall code = 4
 	la $a0, msg2	# load the address of msg
-	syscall
+	syscall         # system_call
     # Get length from user and save
-	li $v0, 5		# read_int syscall code = 5
-	syscall
-	move $a2, $v0	# syscall results returned in $v0
+	li $v0, 7		# read_double syscall code = 7
+	syscall         # system_call
+	mov.d $f16, $f0	# syscall results returned in $f16
+	#-------------------------------------------------------
     jal printLine   # just  print  new  line 
 	#------------------------------------------------------  
 	# Call detect function
-    jal detect		# call detect function 
-	 bne $v1, 0, square		# if $v1 != $zero this is square 
-    	j rec						# else this is rectangle
-        	square:	#square label 
+    jal detect					# Call detect function 
+	 bne $v1, 0, square			# if $v1 != $zero this is square 
+    	j rec					# else this is rectangle
+        	square:				# square label 
 				li $v0, 4       # print_string syscall code = 4
-	            la $a0, msg3   # Print message 'The shape type is: square'
-	            syscall    #return system  call
-	            j exit #jump to exit
-            rec:		#rectangle  lable 
-            	li $v0, 4  # print_string syscall code = 4
-	            la $a0, msg4  # Print message 'The shape type is: rectangle
-	            syscall    #return system  call
+	            la $a0, msg3	# Prints 'The shape type is: square'
+	            syscall    		# return system  call
+	            j end_if 		# jump to end_if
+            rec:				# rectangle  lable 
+            	li $v0, 4  		# print_string syscall code = 4
+	            la $a0, msg4 	# Prints 'The shape type is: rectangle
+	            syscall    		# return system  call
 	#------------------------------------------------------------------------------------------
-	exit : #exit label
-	       jal printLine   # just  print  new  line
+	end_if: 					# exit label
+	       jal printLine   		# Prints a new line
 	#--------------------------------------------------------------------
-	# calculate perimeter 
-	jal perimeter #just  call  perimeter funcation
-    # Prints "The perimeter is: "
-	li $v0, 4    # print_string syscall code = 4
-	la $a0, msg5 #print  message 'the perimeter is :'
-	syscall     #return system  call
+	# Calculate perimeter 
+	jal perimeter 				# Call perimeter function
+	li $v0, 4    				# print_string syscall code = 4
+	la $a0, msg5 				# Prints 'The perimeter is: '
+	syscall     				# return system call
 	# Print perimeter
-	li $v0, 1     # print_string syscall code = 1
-	move $a0, $v1  #print   the  area
-	syscall #return system call
+	li $v0, 3     				# print_double syscall code = 3
+	mov.d $f12, $f0  			# Prints 'The perimeter is: '
+	syscall 					# return system call
     #----------------------------------------------------------------------------------------------
-	jal printLine   # just  call function print  line
+	jal printLine   			# just  call function print  line
 	#---------------------------------------------------------------------------------------------- 
-    # calculate area 
-	jal area   #just call  area function
-    # Prints 'The area is: ' 
-	li $v0, 4 # print_string syscall code = 4
-	la $a0, msg6  #print  message 'the area is :'
-	syscall      #return system  call
+    # Calculate area 
+	jal area   					# Call area function
+	li $v0, 4 					# print_string syscall code = 4
+	la $a0, msg6  				# Prints 'The area is: ' 
+	syscall      				# return system  call
     # Print area
-	li $v0, 1    # print_string syscall code = 1
-	move $a0 ,$v1  #print   the  area 
-	syscall        #return system call
+	li $v0, 3    				# print_double syscall code = 3
+	mov.d $f12 ,$f0  			# Prints 'The area is: '
+	syscall        				# return system call
 	#-----------------------------------------------------------
 	# Exit the program
-	li $v0, 10		# exit
-	syscall #return system call
+	li $v0, 10					# Exit
+	syscall 					# return system call
 #---------------------------------------------------------------------------------------------------
 detect:
-        #TODO  put  here  detect  function  it  take two  paramters  $a1,$a2  and  return  in $v1 if  sqr  -1 if  rec  0
-		beq $a1, $a2, d_square 	#if a1 and a2 are equal jump to equal
-		li $v1,0 				#else the code will continue till 'jr'
-		jr $ra					#return so the code stop excuting at this point
+	# Detect whether the shape is square or rectangle, given width and length
+		c.eq.d $f14, $f16 	# if f14 equals f16 make code = 1
+		bc1t   d_square     # if code equal 1 true
+		li $v1,0 			# else the code will continue till 'jr'
+		jr $ra				# return so the code stop excuting at this point
 	d_square: 
-		   li $v1, 1      # load 1 in  $v1 
-		   jr $ra       # Return to the caller
+		   li $v1, 1      	# load 1 in $v1 
+		   jr $ra       	# Return to the caller
 
 perimeter:
-         #TODO  put  function   perimeter  take  input  in $a1 , $a2  and  return in  $v1
-	add $v1 , $a1 , $a2   # add the width to the length and assign the value to $v1
-    sll $v1 , $v1 , 1	  # shift the sum value 1 bit (left shift) as multiply by 2
-	jr $ra
+	# Calculate the shape perimeter
+	add.d $f0, $f14, $f16 	# add $f14, $f16 in $f0
+    add.d $f0, $f0, $f14  	# add $f14, $f0 in $f0
+    add.d $f0, $f0, $f16  	# add $f16, $f0 in $f0  
+    jr $ra                  # Return to the caller
 area:
-	mul $v1, $a1, $a2 		# Multiply the width by the length and assign the result to $v1
+	# Calculate the shape area
+	mul.d $f0, $f14, $f16 	# Multiply the width by the length and assign the result to $f0
     jr $ra 					# Return to the caller
 printLine:
-      #Print \n
-	li $v0, 4		# print_string syscall code = 4
-	la $a0, newline  # print new  line
-	syscall          #call system function 
-	jr $ra           # return to the  caller
+	#Prints a new line
+	li $v0, 4				# print_string syscall code = 4
+	la $a0, newline  		# print new  line
+	syscall          		# call system function 
+	jr $ra           		# return to the  caller
 #--------------------------------------------------------------          
 # Start .data segment (data!)
 	.data
-#TODO if any one need print message put this here 
 msg1:	.asciiz	"Enter the shape width: "
 msg2:	.asciiz	"Enter the shape length: "
 msg3:	.asciiz	"The shape type is: square"
